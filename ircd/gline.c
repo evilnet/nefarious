e98b22bc0a08666056aea41df46f031dfc20ad47
r33d <devnull@localhost>
2003-10-20 02:09:05 +0000

    Add consideration for when a service does the gline but the reason does not start with a '<'.

--- ircd/gline.c
+++ ircd/gline.c
@@ -229,6 +229,8 @@ do_mangle_gline(struct Client* cptr, struct Client* acptr,
   {
     strcpy(reason, "G-lined by ");
     strncpy(reason, orig_reason+1, endanglebracket - orig_reason + 1);
+  } else if (IsService(sptr) {
+    strcpy(reason, orig_reason);
   } else {
     ircd_snprintf(0, reason, len, "G-lined (<%s> %s)", sptr->cli_name,
 		  orig_reason);
