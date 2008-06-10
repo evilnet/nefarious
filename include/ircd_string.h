8b6075efe4ccf4d1c16980343b974fd37b9cbf2c
sirvulcan <devnull@localhost>
2008-06-10 00:46:03 +0000

    Cleanup of GPL 2 references, switched to GPL 3

--- include/ircd_string.h
+++ include/ircd_string.h
@@ -1,5 +1,22 @@
 /*
- * ircd_string.h
+ * IRC - Internet Relay Chat, include/ircd_string.h
+ * Copyright (C) 1990 Jarkko Oikarinen and
+ *                    University of Oulu, Computing Center
+ * Copyright (C) 1996-1997 Carlo Wood
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  * $Id$
  */
