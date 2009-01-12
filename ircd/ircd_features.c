adc648d8c717235324ddfc58fc10be45093695bc
sirvulcan <devnull@localhost>
2009-01-12 00:52:28 +0000

    Small tweak to DNSBL_LOC_EXEMPT_N_TWO example.

--- ircd/ircd_features.c
+++ ircd/ircd_features.c
@@ -629,7 +629,7 @@ static struct FeatureDesc {
   F_S(LOC_DEFAULT_SERVICE, 0, "AuthServ", 0),
   F_B(DNSBL_LOC_EXEMPT, 0, 0, 0),
   F_S(DNSBL_LOC_EXEMPT_N_ONE, 0, "If you have an account with Nefarious services then you can bypass the DNSBL ban by logging in like this (where Account is your account name and Password is your password):", 0),
-  F_S(DNSBL_LOC_EXEMPT_N_TWO, 0, "Type \002/QUOTE PASS AuthServ Account :Password\002 to connect", 0),
+  F_S(DNSBL_LOC_EXEMPT_N_TWO, 0, "Type /QUOTE PASS AuthServ Account :Password to connect", 0),
   F_B(DNSBL_WALLOPS_ONLY, 0, 0, 0),
   F_B(DNSBL_MARK_FAKEHOST, 0, 1, 0),
   F_B(AUTOINVISIBLE, 0, 0, 0),
