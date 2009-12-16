55cd773cc84e1a7a3dceb60dba0ebeb9e2ddd8e5
sirvulcan <devnull@localhost>
2009-12-16 05:05:23 +0000

    Added some extra checks from an autoscan check. Also if --disable-subversion is given then the svncheck.sh script is skipped on compile.

--- ircd/svncheck.sh
+++ ircd/svncheck.sh
@@ -1,10 +1,12 @@
-SVNSTATUS=`$1/svn status -qu .. | wc -l`
+if [ -f $1/svn ]; then
+	SVNSTATUS=`$1/svn status -qu .. | wc -l`
 
-if (( $SVNSTATUS > "1" )); then
-        echo WARNING
-	echo ""
-	echo One more more of your source files are older than what is
-	echo available on SVN. It is recommended you run svn update to
-	echo get the latest updates.
-	echo ""
+	if (( $SVNSTATUS > "1" )); then
+	        echo WARNING
+		echo ""
+		echo One more more of your source files are older than what is
+		echo available on SVN. It is recommended you run svn update to
+		echo get the latest updates.
+		echo ""
+	fi
 fi
